bca4be1c880601230ec77170cc7265776abe0c61
sgoodale <goodale.steph@gmail.com>
2018-03-03 11:09:07 -0800

    the rock

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ def start():
     board_width = data.get('width')
     board_height = data.get('height')
 
-    head_url = '%s://%s/static/head.png' % (
+    head_url = './therock.png' % (
         bottle.request.urlparts.scheme,
         bottle.request.urlparts.netloc
     )
